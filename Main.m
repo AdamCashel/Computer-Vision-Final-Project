730f7fe61f30de8ea75140e87292a7bbf7aa6bdc
Adam Cashel <67659373+AdamCashel@users.noreply.github.com>
2021-11-26 12:23:21 -0600

    Get images from training_faces

--- Main.m
+++ Main.m
@@ -1,3 +1,19 @@
+Location = 'C:\Users\adamc\OneDrive\Desktop\Final Computer Vision Project\Computer-Vision-Final-Project\training_test_data\training_faces\*bmp';
+ds = imageDatastore(Location);
+length = 3047; %Number of pictures in training_faces
+faces = zeros(100,100,length);
+counter = 1;
+while hasdata(ds)
+    tempImage = read(ds);
+    faces(:,:,counter) = tempImage;
+    
+    counter = counter + 1;
+end
+
+
+
+
+
 face_vertical = 100;
 face_horizontal = 100;
 
@@ -47,5 +63,17 @@ end
 
 %Calling adaboost to find error rates of 25 strong classifiers
 boosted_classifier = AdaBoost(responses, labels, 25);
+
+
+
+% Let's classify a couple of our face and non-face training examples. 
+% A positive prediction value means the classifier predicts the input image to be
+% a face. A negative prediction value means the classifier thinks it's not
+% a face. Values farther away from zero means the classifier is more
+% confident about its prediction, either positive or negative.
+
+prediction = boosted_predict(faces(:, :, 200), boosted_classifier, weak_classifiers, 15)
+
+prediction = boosted_predict(nonfaces(:, :, 500), boosted_classifier, weak_classifiers, 15)
 
 
